c51a53a59697b04402926e0dd04c74cd238cdf54
Hiroki Konishi <hkonishi@yahoo-corp.jp>
2019-06-28 12:50:45 +0900

    Update: use text badge as default

--- badge.go
+++ badge.go
@@ -10,11 +10,11 @@ func addBadge(target string) string {
 	base := path.Base(target)
 	dotIndex := strings.LastIndex(base, ".")
 	if dotIndex < 0 {
-		return " " + target
+		return "  " + target
 	}
 	extension := base[dotIndex+1 : len(base)]
 	if badge, ok := nerdFontMap[extension]; ok {
 		return fmt.Sprintf("%v:%v", badge, target)
 	}
-	return " " + target
+	return fmt.Sprintf("%v:%v", nerdFontMap["txt"], target)
 }
