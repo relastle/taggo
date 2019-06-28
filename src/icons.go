b81bf431b9b258a1dab59ffd189ce9795ea9c36e
Hiroki Konishi <hkonishi@yahoo-corp.jp>
2019-06-28 16:52:06 +0900

    Update: make icon colorable

--- src/icons.go
+++ src/icons.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 	"path"
 	"strings"
+)
+
+// Icon represents one icon
+// which has nerdfont unicode
+// and its color
+type Icon struct {
+	code  string
+	color Color
+}
 
 // The code below is highly inspired by
 // https://github.com/athityakumar/colorls/blob/master/lib/yaml/files.yaml
@@ -11,100 +20,105 @@ import (
 
 // extensionNerdFontMap defines the `extension` -> `nerdfont`
 // mapping.
-var extensionIconMap = map[string]string{
-	"ai":           "\ue7b4",
-	"android":      "\ue70e",
-	"apple":        "\uf179",
-	"audio":        "\uf001",
-	"avro":         "\ue60b",
-	"bash":         "\uf489",
-	"c":            "\ue61e",
-	"clj":          "\ue768",
-	"coffee":       "\uf0f4",
-	"conf":         "\ue615",
-	"cpp":          "\ue61d",
-	"css":          "\ue749",
-	"d":            "\ue7af",
-	"dart":         "\ue798",
-	"db":           "\uf1c0",
-	"diff":         "\uf440",
-	"doc":          "\uf1c2",
-	"docker":       "\uf308",
-	"ebook":        "\ue28b",
-	"env":          "\uf462",
-	"epub":         "\ue28a",
-	"erl":          "\ue7b1",
-	"file":         "\uf15b",
-	"fish":         "\uf489",
-	"font":         "\uf031",
-	"gform":        "\uf298",
-	"git":          "\uf1d3",
-	"go":           "\ue626",
-	"gruntfile.js": "\ue74c",
-	"hs":           "\ue777",
-	"html":         "\uf13b",
-	"image":        "\uf1c5",
-	"iml":          "\ue7b5",
-	"java":         "\ue204",
-	"js":           "\ue74e",
-	"json":         "\ue60b",
-	"jsx":          "\ue7ba",
-	"less":         "\ue758",
-	"log":          "\uf18d",
-	"lua":          "\ue620",
-	"md":           "\uf48a",
-	"mustache":     "\ue60f",
-	"npmignore":    "\ue71e",
-	"pdf":          "\uf1c1",
-	"php":          "\ue73d",
-	"pl":           "\ue769",
-	"ppt":          "\uf1c4",
-	"psd":          "\ue7b8",
-	"py":           "\ue606",
-	"r":            "\uf25d",
-	"rb":           "\ue21e",
-	"rdb":          "\ue76d",
-	"rss":          "\uf09e",
-	"rubydoc":      "\ue73b",
-	"sass":         "\ue603",
-	"scala":        "\ue737",
-	"sh":           "\uf489",
-	"sqlite3":      "\ue7c4",
-	"styl":         "\ue600",
-	"tex":          "\ue600",
-	"ts":           "\ue628",
-	"twig":         "\ue61c",
-	"txt":          "\uf15c",
-	"video":        "\uf03d",
-	"vim":          "\ue62b",
-	"windows":      "\uf17a",
-	"xls":          "\uf1c3",
-	"xml":          "\ue619",
-	"yarn.lock":    "\ue718",
-	"yml":          "\uf481",
-	"zip":          "\uf410",
-	"zsh":          "\uf489",
+var extensionIconMap = map[string]Icon{
+	"ai":           Icon{code: "\ue7b4", color: "yello"},
+	"android":      Icon{code: "\ue70e", color: "black"},
+	"apple":        Icon{code: "\uf179", color: "magenda"},
+	"audio":        Icon{code: "\uf001", color: "black"},
+	"avro":         Icon{code: "\ue60b", color: "black"},
+	"bash":         Icon{code: "\uf489", color: "black"},
+	"c":            Icon{code: "\ue61e", color: "black"},
+	"clj":          Icon{code: "\ue768", color: "black"},
+	"coffee":       Icon{code: "\uf0f4", color: "black"},
+	"conf":         Icon{code: "\ue615", color: "black"},
+	"cpp":          Icon{code: "\ue61d", color: "black"},
+	"css":          Icon{code: "\ue749", color: "black"},
+	"d":            Icon{code: "\ue7af", color: "black"},
+	"dart":         Icon{code: "\ue798", color: "black"},
+	"db":           Icon{code: "\uf1c0", color: "black"},
+	"diff":         Icon{code: "\uf440", color: "black"},
+	"doc":          Icon{code: "\uf1c2", color: "black"},
+	"docker":       Icon{code: "\uf308", color: "black"},
+	"ebook":        Icon{code: "\ue28b", color: "black"},
+	"env":          Icon{code: "\uf462", color: "black"},
+	"epub":         Icon{code: "\ue28a", color: "black"},
+	"erl":          Icon{code: "\ue7b1", color: "black"},
+	"file":         Icon{code: "\uf15b", color: "black"},
+	"fish":         Icon{code: "\uf489", color: "black"},
+	"font":         Icon{code: "\uf031", color: "black"},
+	"gform":        Icon{code: "\uf298", color: "black"},
+	"git":          Icon{code: "\uf1d3", color: "black"},
+	"go":           Icon{code: "\ue626", color: "cyan"},
+	"gruntfile.js": Icon{code: "\ue74c", color: "black"},
+	"hs":           Icon{code: "\ue777", color: "black"},
+	"html":         Icon{code: "\uf13b", color: "black"},
+	"image":        Icon{code: "\uf1c5", color: "black"},
+	"iml":          Icon{code: "\ue7b5", color: "black"},
+	"java":         Icon{code: "\ue204", color: "black"},
+	"js":           Icon{code: "\ue74e", color: "yellow"},
+	"json":         Icon{code: "\ue60b", color: "yellow"},
+	"jsx":          Icon{code: "\ue7ba", color: "yellow"},
+	"less":         Icon{code: "\ue758", color: "black"},
+	"log":          Icon{code: "\uf18d", color: "black"},
+	"lua":          Icon{code: "\ue620", color: "yellow"},
+	"md":           Icon{code: "\uf48a", color: "black"},
+	"mustache":     Icon{code: "\ue60f", color: "black"},
+	"npmignore":    Icon{code: "\ue71e", color: "black"},
+	"pdf":          Icon{code: "\uf1c1", color: "black"},
+	"php":          Icon{code: "\ue73d", color: "red"},
+	"pl":           Icon{code: "\ue769", color: "black"},
+	"ppt":          Icon{code: "\uf1c4", color: "black"},
+	"psd":          Icon{code: "\ue7b8", color: "black"},
+	"py":           Icon{code: "\ue606", color: "yellow"},
+	"r":            Icon{code: "\uf25d", color: "black"},
+	"rb":           Icon{code: "\ue21e", color: "red"},
+	"rdb":          Icon{code: "\ue76d", color: "black"},
+	"rss":          Icon{code: "\uf09e", color: "black"},
+	"rubydoc":      Icon{code: "\ue73b", color: "red"},
+	"sass":         Icon{code: "\ue603", color: "black"},
+	"scala":        Icon{code: "\ue737", color: "black"},
+	"sh":           Icon{code: "\uf489", color: "black"},
+	"sqlite3":      Icon{code: "\ue7c4", color: "blue"},
+	"styl":         Icon{code: "\ue600", color: "black"},
+	"tex":          Icon{code: "\ue600", color: "black"},
+	"ts":           Icon{code: "\ue628", color: "yellow"},
+	"twig":         Icon{code: "\ue61c", color: "black"},
+	"txt":          Icon{code: "\uf15c", color: "black"},
+	"video":        Icon{code: "\uf03d", color: "black"},
+	"vim":          Icon{code: "\ue62b", color: "blue"},
+	"windows":      Icon{code: "\uf17a", color: "black"},
+	"xls":          Icon{code: "\uf1c3", color: "black"},
+	"xml":          Icon{code: "\ue619", color: "black"},
+	"yarn.lock":    Icon{code: "\ue718", color: "black"},
+	"yml":          Icon{code: "\uf481", color: "black"},
+	"zip":          Icon{code: "\uf410", color: "black"},
+	"zsh":          Icon{code: "\uf489", color: "black"},
 }
 
-var basenameIconMap = map[string]string{
-	"Dockerfile": "\ue7b0",
+var basenameIconMap = map[string]Icon{
+	"Dockerfile": Icon{code: "\ue7b0", color: "cyan"},
 }
 
-func addIcon(target string) string {
+func getIcon(target string) Icon {
 	base := path.Base(target)
 	// if base name mathes to something, return that.
 	if icon, ok := basenameIconMap[base]; ok {
-		return fmt.Sprintf("%v:%v", icon, target)
+		return icon
 	}
 
 	// No extension is assumed as simple text file
 	dotIndex := strings.LastIndex(base, ".")
 	if dotIndex < 0 {
-		return fmt.Sprintf("%v:%v", extensionIconMap["txt"], target)
+		return extensionIconMap["txt"]
 	}
 	extension := base[dotIndex+1 : len(base)]
 	if icon, ok := extensionIconMap[extension]; ok {
-		return fmt.Sprintf("%v:%v", icon, target)
+		return icon
 	}
-	return fmt.Sprintf("%v:%v", extensionIconMap["txt"], target)
+	return extensionIconMap["txt"]
+}
+
+func addIcon(target string) string {
+	icon := getIcon(target)
+	return fmt.Sprintf(ColorFuncMap[icon.color](icon.code))
 }
